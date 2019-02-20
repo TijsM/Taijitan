f4eef6f7c51c90b1fce414a2606f9ba00a9464a9
Jarne Deschacht <jarne.deschacht@student.hogent.be>
2019-02-20 16:02:01 +0100

    translate everything to English

--- Taijitan/Data/ApplicationDbContext.cs
+++ Taijitan/Data/ApplicationDbContext.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Taijitan.Models.Domain;
 
 namespace Taijitan.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Lid> Leden { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new MemberConfiguration());
         }
     }
 }
---
+++ Taijitan/Models/Domain/Member.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taijitan.Models.Domain
+{
+    public class Member
+    {
+        public string Name { get; set; }
+        public string Firstname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Member(string naam, string voornaam, DateTime geboortedatum, string adres, int telefoonnummer, string email)
+        {
+            Name = naam;
+            Firstname = voornaam;
+            DateOfBirth = geboortedatum;
+            Address = adres;
+            PhoneNumber = telefoonnummer;
+            Email = email;
+        }
+
+        public void WijzigGegevens(string naam, string voornaam, DateTime geboortedatum, string adres, int telefoonnummer, string email)
+        {
+            Name = naam;
+            Firstname = voornaam;
+            DateOfBirth = geboortedatum;
+            Address = adres;
+            PhoneNumber = telefoonnummer;
+            Email = email;
+        }
+
+    }
+}
