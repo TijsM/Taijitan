e908139a6b159ca53dc3c9375c3e25bde618b9c9
Tijs Martens <tijs.martens@student.hogent.be>
2019-02-20 20:11:08 +0100

    added validation in Member

--- Taijitan/Models/Domain/Member.cs
+++ Taijitan/Models/Domain/Member.cs
@@ -7,18 +7,70 @@ namespace Taijitan.Models.Domain
 {
     public class Member
     {
+
+        #region Fields
+        private string _name;
+        private string _firstName;
+        private DateTime _dateOfBirth;
+        private string _phoneNumber;
+        private string _email;
+        #endregion
+
+
         public int MemberId { get; set; }
-        public string Name { get; set; }
-        public string FirstName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public string Name {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("City must have a name");
+                _name = value;
+            }
+        }
+        public string FirstName {
+            get => _firstName;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("City must have a name");
+                _firstName = value;
+            }
+              
+        }
+        public DateTime DateOfBirth {
+            get => _dateOfBirth;
+            set
+            {
+                if (_dateOfBirth > DateTime.Today)
+                    throw new ArgumentException("City must have a name");
+                _dateOfBirth = value;
+            }
+        }
         public string Street { get; set; }
         public City City { get; set; }
         public string Country { get; set; }
         public string HouseNumber { get; set; }
-        public int PhoneNumber { get; set; }
-        public string Email { get; set; }
+        public string PhoneNumber {
+            get => _phoneNumber;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("City must have a name");
+                _phoneNumber = value;
+            }
+        }
+  
+        public string Email {
+            get => _email;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("City must have a name");
+                _email = value;
+            }
+        }
 
-        public Member(string name, string firstName, DateTime dateOfBirth,string street,City city,string country,string houseNumber,int phoneNumber, string email)
+        public Member(string name, string firstName, DateTime dateOfBirth,string street,City city,string country,string houseNumber,string phoneNumber, string email)
         {
             Name = name;
             FirstName = firstName;
@@ -32,7 +84,7 @@ namespace Taijitan.Models.Domain
         }
         private Member() { }
 
-        public void Change(string name, string firstName, DateTime dateOfBirth, string street, City city, string country, string houseNumber, int phoneNumber, string email)
+        public void Change(string name, string firstName, DateTime dateOfBirth, string street, City city, string country, string houseNumber, string phoneNumber, string email)
         {
             Name = name;
             FirstName = firstName;
