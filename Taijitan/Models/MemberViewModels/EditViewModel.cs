a72231f224c20da016a8a37879f351281c451a27
Tijs Martens <tijs.martens@student.hogent.be>
2019-02-21 11:08:48 +0100

    added EditViewModel of Member

---
+++ Taijitan/Models/MemberViewModels/EditViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taijitan.Models.Domain;
+
+namespace Taijitan.Models.MemberViewModels
+{
+    public class EditViewModel
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Street { get; set; }
+        public string PostalCode { get; set; }
+        public string CityName { get; set; }
+        public string Country { get; set; }
+        public string HouseNumber { get; set; }
+        public string PhonenNumber { get; set; }
+        public string Email { get; set; }
+
+
+        public EditViewModel(Member member)
+        {
+            Name = member.Name;
+            FirstName = member.FirstName;
+            DateOfBirth = member.DateOfBirth;
+            Street = member.Street;
+            PostalCode = member.City?.Postalcode ?? "Not Found";
+            CityName = member.City?.Name ?? "Not Found";
+            Country = member.Country;
+            HouseNumber = member.HouseNumber;
+            Email = member.Email;
+        }
+    }
+}
