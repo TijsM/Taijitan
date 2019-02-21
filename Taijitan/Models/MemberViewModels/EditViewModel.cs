c2bfcff8d6c5a7aa460cd3bd714609d9b1f34504
Tijs Martens <tijs.martens@student.hogent.be>
2019-02-21 11:48:53 +0100

    added annotations in the edditViewModel

--- Taijitan/Models/MemberViewModels/EditViewModel.cs
+++ Taijitan/Models/MemberViewModels/EditViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Taijitan.Models.Domain;
@@ -8,15 +9,42 @@ namespace Taijitan.Models.MemberViewModels
 {
     public class EditViewModel
     {
+        [Required(ErrorMessage = "{0} is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name = "Date of birth")]
+        [DataType(DataType.Date)]
         public DateTime DateOfBirth { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name = "Streetname")]
         public string Street { get; set; }
+
+        [DataType(DataType.PostalCode)]
+        [Required(ErrorMessage = "{0} is required")]
         public string PostalCode { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name = "City")]
         public string CityName { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
         public string Country { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Display(Name = "House number")]
         public string HouseNumber { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.PhoneNumber)]
         public string PhonenNumber { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
 
