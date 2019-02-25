2b0a872c79eae0e020a7bfbaac4d3f127e1019d9
Tijs Martens <tijs.martens@student.hogent.be>
2019-02-25 15:35:12 +0100

    added enum formule

---
+++ Taijitan/Models/Domain/Formule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taijitan.Models.Domain
+{
+    public enum Formule
+    {
+        [Display(Name="dinsdag en donderdag")]
+        formule1,
+
+        [Display(Name = "dinsdag en zaterdag")]
+        formule2,
+
+        [Display(Name = "woensdag en zaterdag")]
+        formule3,
+
+        [Display(Name = "woensdag")]
+        formule4,
+        
+        [Display(Name = "zaterdag")]
+        formule5,
+
+        [Display(Name = "deelname aan activiteit")]
+        formule6,
+
+        [Display(Name = "deelname meerdaagse stage")]
+        formule7
+
+
+
+    }
+}
--- TaijitanTest/Controllers/UserControllerTest.cs
+++ TaijitanTest/Controllers/UserControllerTest.cs
@@ -17,7 +17,9 @@ namespace TaijitanTest.Controllers
         private readonly UserController _userController;
         private readonly Mock<IUserRepository> _mockUserRepository;
         private readonly Mock<ICityRepository> _mockCityRepository;
-        private readonly UserManager<IdentityUser> _userManager;
+
+        private readonly Mock<UserManager<IdentityUser>> _userManager;
+
 
         private readonly User _tomJansens;
         private readonly int _tomJansensId;
@@ -31,9 +33,9 @@ namespace TaijitanTest.Controllers
             _dummyContext = new DummyApplicationDbContext();
             _mockUserRepository = new Mock<IUserRepository>();
             _mockCityRepository = new Mock<ICityRepository>();
-            _userManager = new Mock<UserManager<IdentityUser>>().Object;
+            _userManager = new Mock<UserManager<IdentityUser>>();
             _userController = new UserController
-                (_mockUserRepository.Object, _mockCityRepository.Object, _userManager);
+                (_mockUserRepository.Object, _mockCityRepository.Object, _userManager.Object);
 
             _tomJansens = _dummyContext.UserTomJansens;
             _tomJansensId = _tomJansens.UserId;
@@ -44,7 +46,7 @@ namespace TaijitanTest.Controllers
         [Fact]
         public void EddiHttpGet_ValidProductId_PassesUsersDetails()
         {
-            var result = _userController.Edit() as ViewResult;
+            var result = _userController.Edit(_tomJansensId) as ViewResult;
             result.ViewData["userId"] = _tomJansensId;
             var userViewModel = result?.Model as EditViewModel;
             Assert.Equal("Tom", userViewModel?.FirstName);
