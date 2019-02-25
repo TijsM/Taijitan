8300440af26e29ca750fe80eaa76e0d11f481494
Tijs Martens <tijs.martens@student.hogent.be>
2019-02-25 16:29:44 +0100

    Fixed typo

--- Taijitan/Models/Domain/Formula.cs
+++ Taijitan/Models/Domain/Formula.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Taijitan.Models.Domain
 {
-    public enum Formule
+    public enum Formula
     {
         [Display(Name="dinsdag en donderdag")]
         Formule1,
