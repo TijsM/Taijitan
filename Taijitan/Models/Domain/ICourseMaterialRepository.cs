199b083b19d27567bba3e9696ed6f67b5c36265b
Jarne Deschacht <jarne.deschacht@student.hogent.be>
2019-03-11 13:21:40 +0100

    added repository for CourseMaterial

---
+++ Taijitan/Models/Domain/ICourseMaterialRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Taijitan.Models.Domain
+{
+    public interface ICourseMaterialRepository
+    {
+        IEnumerable<CourseMaterial> GetAll();
+        IEnumerable<CourseMaterial> GetByRank(Rank rank);
+        void Add(CourseMaterial courseMaterial);
+        void Delete(CourseMaterial courseMaterial);
+        void SaveChanges();
+    }
+}
