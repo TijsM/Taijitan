40f74bc518bae35eaae1cf49e91d54c29c125314
Stef Verlinde <stef.verlinde@student.hogent.be>
2019-03-19 21:49:53 +0100

    Working on sessionControllerTest

--- TaijitanTest/Controllers/SessionControllerTest.cs
+++ TaijitanTest/Controllers/SessionControllerTest.cs
@@ -19,13 +19,24 @@ namespace TaijitanTest.Controllers
     {
 
         #region Fields
-        private readonly SessionController _sessionController;
         private readonly DummyApplicationDbContext _dummyContext;
-        private readonly Mock<IUserRepository> _mockUserRepository;
-        private readonly Mock<ISessionRepository> _mockSessionRepository;
-        private readonly Mock<IFormulaRepository> _mockFormulaRepository;
-        private readonly Mock<ITrainingDayRepository> _mockTrainingDayRepository;
-        private readonly Mock<INonMemberRepository> _nonMemberRepository;
+        private SessionController _sessionController;
+        private Mock<IUserRepository> _mockUserRepository;
+        private Mock<ISessionRepository> _mockSessionRepository;
+        private Mock<IFormulaRepository> _mockFormulaRepository;
+        private Mock<ITrainingDayRepository> _mockTrainingDayRepository;
+        private Mock<INonMemberRepository> _nonMemberRepository;
+        private Member _tomJansens;
+        private Session _session1;
+        private string _tomJansenCityPostalCode;
+        private int _tomJansensId;
+        private int _session1Id;
+        private string _NonExistingPostalCode;
+        private int _NonExistingUserId;
+        private string _tomJansensEmail;
+        private Formula _tomJansensFormula;
+        private string _partOfName;
+        private Admin _alain;
         #endregion
 
 
@@ -38,9 +49,34 @@ namespace TaijitanTest.Controllers
             _mockFormulaRepository = new Mock<IFormulaRepository>();
             _mockTrainingDayRepository = new Mock<ITrainingDayRepository>();
             _nonMemberRepository = new Mock<INonMemberRepository>();
+            _tomJansens = _dummyContext.UserTomJansens;
+            _session1 = _dummyContext.Session1;
+            _tomJansensId = _tomJansens.UserId;
+            _session1Id = _session1.SessionId;
+            _tomJansenCityPostalCode = _tomJansens.City.Postalcode;
+            _NonExistingPostalCode = "9999";
+            _NonExistingUserId = 9999;
+            _partOfName = "de";
+            _tomJansensEmail = _tomJansens.Email;
+            _tomJansensFormula = _tomJansens.Formula;
+            _alain = _dummyContext.Alain;
 
+            //Setups
+            _mockUserRepository.Setup(c => c.GetAll()).Returns(_dummyContext.Users);
+            _mockUserRepository.Setup(c => c.GetById(_tomJansensId)).Returns(_dummyContext.UserTomJansens);
+            _mockUserRepository.Setup(c => c.GetByEmail(_tomJansensEmail)).Returns(_dummyContext.UserTomJansens);
+            _mockUserRepository.Setup(c => c.GetByFormula(_tomJansensFormula)).Returns(_dummyContext.UsersFormula1);
+            _mockUserRepository.Setup(c => c.GetByPartofName(_partOfName)).Returns(_dummyContext.UsersByPartOfName);
 
-            _sessionController = new SessionController(_mockUserRepository.Object, _mockSessionRepository.Object, _mockFormulaRepository.Object, _mockTrainingDayRepository.Object,_nonMemberRepository.Object)
+            _mockSessionRepository.Setup(c => c.GetAll()).Returns(_dummyContext.Sessions);
+            _mockSessionRepository.Setup(c => c.GetById(_session1Id)).Returns(_dummyContext.Session1);
+
+            _mockFormulaRepository.Setup(c => c.GetAll()).Returns(_dummyContext._formulas);
+            _mockFormulaRepository.Setup(c => c.GetByTrainingDay);
+            //_mockTrainingDayRepository;
+            //_nonMemberRepository;
+
+        _sessionController = new SessionController(_mockUserRepository.Object, _mockSessionRepository.Object, _mockFormulaRepository.Object, _mockTrainingDayRepository.Object,_nonMemberRepository.Object)
             {
                 TempData = new Mock<ITempDataDictionary>().Object
             };
