58b178c553d6581f8d1606ccd919f2d18f9960f1
Tijs Martens <tijs.martens@student.hogent.be>
2019-03-19 21:51:52 +0100

    added Course

---
+++ TaijitanTest/Controllers/CourseMaterialControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Taijitan.Models.Domain;
+
+namespace TaijitanTest.Controllers
+{
+    class CourseMaterialControllerTest
+    {
+        
+        private readonly Mock<ISessionRepository> _mockSessionRepository;
+        private readonly Mock<IUserRepository> _mockUserRepository;
+        private readonly Mock<ICourseMaterialRepository> _mockCourseMaterialRepository;
+        private readonly Mock<ICommentRepository> _MockCommentRepository;
+
+    }
+}
