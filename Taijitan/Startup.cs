e369a3124acb17afb8f831f5e36f69c060d85168
Robbe Dekien <robbe.dekien@student.hogent.be>
2019-03-27 18:56:49 +0100

    Changed back to dev mode

--- Taijitan/Startup.cs
+++ Taijitan/Startup.cs
@@ -45,7 +45,8 @@ namespace Taijitan
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
 
-            services.AddAuthorization(options => {
+            services.AddAuthorization(options =>
+            {
                 options.AddPolicy("Admin", policy => policy.RequireClaim(ClaimTypes.Role, "Admin"));
                 options.AddPolicy("Teacher", policy => policy.RequireClaim(ClaimTypes.Role, "Teacher"));
                 options.AddPolicy("Member", policy => policy.RequireClaim(ClaimTypes.Role, "Member"));
@@ -53,7 +54,7 @@ namespace Taijitan
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddScoped<TaijitanDataInitializer>();
-            services.AddScoped<IUserRepository,UserRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<ICityRepository, CityRepository>();
             services.AddScoped<ISessionRepository, SessionRepository>();
             services.AddScoped<ITrainingDayRepository, TrainingDayRepository>();
@@ -71,19 +72,19 @@ namespace Taijitan
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env,TaijitanDataInitializer dataInitializer)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, TaijitanDataInitializer dataInitializer)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //    app.UseDatabaseErrorPage();
-            //}
-            //else
-            //{
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
                 app.UseExceptionHandler("/Errors"); //Catcht runtime errors in de code
                 app.UseStatusCodePagesWithReExecute("/Errors/Error/{0}"); //Catch errors 404
                 app.UseHsts();
-            //}
+            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
@@ -95,9 +96,9 @@ namespace Taijitan
 
             app.UseMvc(routes =>
             {
-            routes.MapRoute(
-                name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "default",
+                        template: "{controller=Home}/{action=Index}/{id?}");
             });
             dataInitializer.InitializeData().Wait();
         }
